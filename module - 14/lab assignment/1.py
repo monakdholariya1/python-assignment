2ec01aa1a12f767201d4d24815c3725d6f62c462
monakdholariya1 <monakdholariya889@gmail.com>
2025-10-07 10:00:32 +0530

    1.py

--- module - 14/lab assignment/1.py
+++ module - 14/lab assignment/1.py
@@ -1,2 +1,12 @@
-lst = ["monak",25,25.11,[1,2,3]]
-print(lst)
+#Write a Python program to create a list with elements of multiple data types (integers,strings, floats, etc.).
+
+lst = [20,"monank",4.56,[10,20],True] 
+
+# find lst data typs
+print(type(lst))
+
+#find all element data type
+for i in range(len(lst)):
+    print(type(lst[i]))
+
+           
